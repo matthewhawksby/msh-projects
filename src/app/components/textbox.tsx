fe2e014c2b6699804d75175b30a1426e04b1a419
Matthew Hawksby <103611981+MatthewHawksbyGithub@users.noreply.github.com>
2025-02-17 15:27:56 -0800

    remove misc

--- src/app/components/textbox.tsx
+++ src/app/components/textbox.tsx
@@ -5,11 +5,11 @@ import React from "react";
 const FloatingTextbox: React.FC = () => {
   return (
     <div className="relative">
-      <div className="absolute top-[10vh] left-1/2 transform -translate-x-1/2 
+      <div className="absolute top-[10vh] left-1/3 transform -translate-x-1/2 
                       w-11/12 sm:w-4/5 md:w-3/4 lg:w-2/3 xl:w-1/2 max-w-5xl 
                       min-h-[500px] sm:min-h-[500px] md:min-h-[500px] 
                       p-6 bg-white bg-opacity-80 backdrop-blur-sm shadow-sm rounded-lg 
-                      z-50 text-lg leading-relaxed text-gray-900">
+                      z-10 text-lg leading-relaxed text-gray-900">
         <p className="font-body text-lg">
           My name is Matthew Hawksby, a Computer Science and Mathematics student at Simon Fraser University in BC Canada. This website is a work in progress!
         </p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,18 +49,13 @@ export default function Home() {
           <Menu></Menu>
         </div>
       </nav>
-
-
         <div className="font-body justify-center sm:pr-4 sm:pl-4 md:pl-12 md:pr-12">
           <FloatingTextbox></FloatingTextbox>
           <Grid></Grid>
         </div>
       <div className="justify-center sm:pr-4 sm:pl-4 md:pl-40 md:pr-40">
+                  <FloatingTextbox></FloatingTextbox>
         <div>
-          <div className="bg-emerald-200 p-12 rounded-lg text-2xl pt-12 md:max-2xl:pb-12 font-display h-48 md:max-2xl:ml-10 md:max-2xl:text-4xl text-gray-1000 font-bold">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </div>
-          <div className="bg-red-50 h-72 md:w-1/3">Testing</div>
           <ScrollBox
             bgColor="bg-emerald-500"
             width={10}
@@ -68,87 +63,8 @@ export default function Home() {
           ></ScrollBox>
           <div className="flex justify-center pt-6 font-dm-sans w-full h-32 pt-24 md:max-2xl:w-1/2 text-center text-gray-1000 font-bold border-tetraRed border-b-2 text-lg lg:pb-12 md:max-2xl:pt-10 md:max-2xl:pl-6">
             Always interested in learning new things.
-            <br></br>I hope to find ways to bring substantial value to the
-            companies and organizations that I work for in the future.
-          </div>
-          <div className="flex justify-center">
-            <div className="grid pt-12 mb-32 text-center lg:mb-0 lg:w-full justify-evenly gap-4 lg:max-w-5xl sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:text-left">
-              <a
-                href=""
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 
-              hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <h2 className="mb-3 text-2xl font-semibold">
-                  Projects{" "}
-                  <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                    -&gt;
-                  </span>
-                </h2>
-                <p className="m-0 max-w-[30ch] text-sm opacity-50">
-                  First Text Box is located here. Create link to Projects page.
-                </p>
-              </a>
-              <a
-                href=""
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 
-              hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <h2 className="mb-3 text-2xl font-semibold">
-                  Blog{" "}
-                  {/* NOTE : The group-hover translate item down there is fun.
-                    TODO:: These things are so neat.*/}
-                  <span className="inline-block transition-transform group-hover:translate-y-10 motion-reduce:transform-none">
-                    -&gt;
-                  </span>
-                </h2>
-                <p className="m-0 max-w-[30ch] text-sm opacity-50">
-                  Second Text Box. Link to Blog.
-                </p>
-              </a>
-
-              <a
-                href=""
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 
-              hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <h2 className="mb-3 text-2xl font-semibold">
-                  About{" "}
-                  <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                    -&gt;
-                  </span>
-                </h2>
-                <p className="m-0 max-w-[30ch] text-sm opacity-50">
-                  Third Text Box, link to About information.
-                </p>
-              </a>
-
-              <a
-                href=""
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 
-              hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <h2 className="mb-3 text-2xl font-semibold">
-                  Links{" "}
-                  <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                    -&gt;
-                  </span>
-                </h2>
-                <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-                  https://picaq.github.io/sarasa/
-                </p>
-              </a>
-            </div>
           </div>
         </div>
-
       </div>
       <nav className="flex md:pr-32 md:pl-32 h-10 w-screen bg-neutral-200 items-center justify-between border-gray-900/50 bg-white border-t-2 sticky bottom-0 z-50"></nav>
     </main>
