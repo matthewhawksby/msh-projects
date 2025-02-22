e72d5d46a41676fa5ac6f79d5f2d337b5a211de9
MatthewHawksby <hawkesbee@yahoo.ca>
2025-02-21 16:16:32 -0800

    test add

--- src/app/components/textbox.tsx
+++ src/app/components/textbox.tsx
@@ -9,7 +9,7 @@ interface FloatingTextboxProps {
 const FloatingTextbox: React.FC<FloatingTextboxProps> = ({ children }) => {
   return (
   <div className="absolute flex p-6 justify-center items-center bg-white bg-opacity-80 shadow-sm rounded-lg text-gray-900 z-10">
-    {children}
+    {children}p
   </div>  
   );
 };
