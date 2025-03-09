5fc35e4c6852538a53e88ef37f213c76d8e78f85
MatthewHawksby <103611981+matthewhawksby@users.noreply.github.com>
2025-03-08 19:50:25 -0800

    add link to stitch

--- src/app/components/projectBox.tsx
+++ src/app/components/projectBox.tsx
@@ -1,22 +1,25 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Link from "next/link";
 
-const ProjectBox = ({ image, title, description }: { image: string; title: string; description: string }) => {
+const ProjectBox = ({ image, title, description }: { image: string; title: string; description: string;}) => {
   return (
-    <motion.div 
+    <motion.div
       className="relative w-[90%] max-w-lg rounded-3xl overflow-hidden shadow-xl bg-white border border-gray-300 hover:shadow-2xl transition-transform duration-300"
-      whileHover={{ scale: 1.08 }}
+      whileHover={{ scale: 1.04 }}
     >
-        <div className="p-3">
-<div className="border-2 border-black rounded-3xl overflow-hidden">
-  <img src={image} alt={title} className="w-full h-[250px] object-cover rounded-2xl" />
-</div>
-</div>
-      <div className="p-6">
-        <h3 className="text-xl font-bold">{title}</h3>
-        <p className="text-gray-700 text-md mt-2">{description}</p>
+      <div className="p-3">
+        <Link href="./projects/projpages/stitch">
+        <div className="border-2 border-black rounded-3xl overflow-hidden">
+          <img src={image} alt={title}  className="w-full h-[250px] object-cover rounded-2xl" />
+        </div>
+        </Link>
       </div>
+        <div className="p-6">
+          <h3 className="text-xl font-bold">{title}</h3>
+          <p className="text-gray-700 text-md mt-2">{description}</p>
+        </div>
     </motion.div>
   );
 };
--- src/app/projects/projpages/stitch/page.tsx
+++ src/app/projects/projpages/stitch/page.tsx
@@ -1,11 +1,11 @@
-import Dropdown from "../../components/dropdown";
-import Logo from "../../components/logo";
-import Menu from "../../components/menu";
-import Grid from "../../components/grid";
-import ScrollBox from "../../components/scrollbox";
-import FloatingTextbox from "../../components/textbox";
+import Dropdown from "../../../components/dropdown";
+import Logo from "../../../components/logo";
+import Menu from "../../../components/menu";
+import Grid from "../../../components/grid";
+import ScrollBox from "../../../components/scrollbox";
+import FloatingTextbox from "../../../components/textbox";
 
-export default function StitchPage() {
+export default function Home() {
   return (
     <main className="w-screen bg-gray-100">
       <nav className="flex md:pr-32 md:pl-32 w-screen bg-neutral-200 items-center justify-between border-gray-900/50 bg-white border-b-2 sticky top-0 z-50">
